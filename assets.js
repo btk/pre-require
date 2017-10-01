cd8873126b6bd245fd8a2d626055cec59cbe704e
Syed Faraaz Ahmad <faraaz98@live.com>
2017-10-01 14:59:52 +0530

    enable generation of assets.js with search function

--- assets.js
+++ assets.js
@@ -1,15 +1,13 @@
 // import this file for static assets
-let Assets = {
-  asda_sda_json: require('./asda-sda.json'),
-  asdasda_json: require('./asdasda.json'),
-  qweqwe_json: require('./qweqwe.json'),
-  vcvcvcv_json: require('./vcvcvcv.json'),
-  search: function(key) {
-    if(this.hasOwnProperty(key))
-      return Assets[key];
-    else
-      return -1;
+  let Obj = {
+    asda_sda_json: require('./images/asda-sda.json'), asdasda_json: require('./images/asdasda.json'), qweqwe_json: require('./images/qweqwe.json'), vcvcvcv_json: require('./images/vcvcvcv.json'),
+    search: function(key) {
+      if(this.hasOwnProperty(key)) {
+        return this[key]
+      }
+      else
+        return -1;
+    }
   }
-}
-
-export default Assets;
+  export default Obj;
+  
