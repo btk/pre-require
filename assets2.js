a2187f640eef3f352bfc68d9bb4435b484895041
hanstf <hansbrians@gmail.com>
2017-10-08 11:43:19 +0800

    add select assets recursively

---
+++ assets2.js
@@ -0,0 +1,30 @@
+// import this file for static assets
+  let Obj = {
+    asas:{
+        asasas:{
+            tsconfig_json:require('./images/asas/asasas/tsconfig.json')
+        },
+        tsconfig_json:require('./images/asas/tsconfig.json')
+    },
+      asda_sda_json:require('./images/asda-sda.json'),
+      asdasda_json:require('./images/asdasda.json'),
+      qweqwe_json:require('./images/qweqwe.json'),
+      vcvcvcv_json:require('./images/vcvcvcv.json'),
+    search: function(key) {
+      if(this.hasOwnProperty(key)) {
+        return this[key];
+      } else {
+        return -1;
+      }
+    },
+    format: function(extention) {
+      let filteredArray = this.filter(assetSlug => (assetSlug.includes("-"+extention)));
+      if(filteredArray.length){
+        return filteredArray;
+      } else {
+        return -1;
+      }
+    }
+  }
+  export default Obj;
+  
