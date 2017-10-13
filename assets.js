030f6eec3fd755497732a7e48b8c4280f3d5ded4
Burak Tokak <iletisim@buraktokak.com>
2017-10-14 02:18:05 +0300

    Handle transpiling es2015

--- assets.js
+++ assets.js
@@ -1,20 +1,22 @@
 // import this file for static assets
   let Obj = {
     asda_sda_json: require('./test-assets/asda-sda.json'), asdasda_json: require('./test-assets/asdasda.json'), data_json: require('./test-assets/data.json'), image_a_jpg: require('./test-assets/image-a.jpg'), image_a_png: require('./test-assets/image-a.png'), image_b_png: require('./test-assets/image-b.png'), qweqwe_json: require('./test-assets/qweqwe.json'), vcvcvcv_json: require('./test-assets/vcvcvcv.json'),
-    search: function (key) {
-      if(this.hasOwnProperty(key)) {
-        return this[key];
-      } else {
-        return -1;
-      }
-    },
-    format: function (extension) {
-      let filteredArray = this.filter(assetSlug => (assetSlug.includes("-"+extention)));
-      if(filteredArray.length){
-        return filteredArray;
-      } else {
-        return -1;
-      }
-    }
+    search: function searchFile(key) {
+  if (this.hasOwnProperty(key)) {
+    return this[key];
+  } else {
+    return -1;
+  }
+},
+    format: function searchFileFormat(extension) {
+  var filteredArray = this.filter(function (assetSlug) {
+    return assetSlug.includes("-" + extention);
+  });
+  if (filteredArray.length) {
+    return filteredArray;
+  } else {
+    return -1;
+  }
+}
   }
   export default Obj;
