4b58b35ceac5905db7547bc4ae9a41d15b9a0353
Paul Barry <devlab2425@gmail.com>
2017-10-17 23:40:07 -0400

    Stub out dependencies

--- tests/loop-directory.test.js
+++ tests/loop-directory.test.js
@@ -1,9 +1,46 @@
-import { loopDirectory } from '../index.js'
-import { assetsLength } from './utils'
+import {loopDirectory} from '../lib/loop-directory'
+import {assetsLength} from './utils'
+
+jest.mock('fs', () => {
+    const items = ['path/to/file/1', 'path/to/file/2', 'path/to/file/3'];
+
+    return {
+        readdir: jest.fn((path, cb) => {
+            cb(null, items)
+        }),
+        lstatSync: jest.fn().mockReturnValue({
+            isDirectory: jest.fn()
+        })
+    };
+});
+
+import fs from 'fs';
 
 describe('Loop directory Test', () => {
-  it('Should return the files length in folder', async () => {
-    const assets = await loopDirectory('./test-assets', {});
-    expect(assetsLength(assets)).toBe(10);
-  })
-})
+
+    let directoryItems;
+
+    beforeEach(() => {
+        directoryItems = ['path/to/file/1', 'path/to/file/2', 'path/to/file/3'];
+    });
+
+    test('should return a Promise', () => {
+        expect(loopDirectory('./test-assets', {}).then).toBeDefined();
+    });
+
+    test('should read the directory', () => {
+        const spy = jest.spyOn(fs, 'readdir');
+        const filePath = './test-assets';
+
+        loopDirectory(filePath, {});
+
+        expect(spy).toHaveBeenCalledWith(filePath, expect.any(Function));
+    });
+
+    test('Should return the files length in folder', async () => {
+        const assets = await loopDirectory('./test-assets', {});
+
+        expect(assetsLength(assets)).toEqual(directoryItems.length);
+    });
+
+});
